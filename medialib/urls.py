29b4c378d525f4664d669906a8613331f25f7187
Eduardo <eduardogm33@outlook.es>
2026-05-01 11:17:30 +0100

    feat: añadida la url para el manejo de la página de resultados de búsqueda

--- medialib/urls.py
+++ medialib/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path("user/create", views.register, name="create.user"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"), 
+    path("dashboard/<int:user_id>", views.dashboard, name="dashboard"),
+    path("search-results", views.search_results, name="search.results"),
 ]
