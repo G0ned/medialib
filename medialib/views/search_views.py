32fe05bea625e10a298c70b225f642106ad20650
Eduardo <eduardogm33@outlook.es>
2026-05-01 10:54:26 +0100

    feat: creada vista para manejar las búsquedas

---
+++ medialib/views/search_views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from ..services import search_videogames
+
+def search_results(request):
+    #TODO: Implement search results page
+    #1. Retrieve the search query from the GET parameters of the request
+    query = request.GET.get("search")
+    #2. If the search query is empty, redirect the user to the home page
+    if not query:
+        return redirect("medialib:home")
+    #3. If the search query is not empty, call the search_videogames service with the search query and render the search results
+    results = search_videogames(query)
+    return render(request, "medialib/components/search_results.html", {"result": results})
