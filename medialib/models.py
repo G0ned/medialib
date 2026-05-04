0fcc6d589fd67f2aed3b96dea128e54cc8b459f3
Eduardo <eduardogm33@outlook.es>
2026-05-04 17:20:09 +0100

    refactor: nombres de videojuegos y plataformas ahora son unique

--- medialib/models.py
+++ medialib/models.py
@@ -21,13 +21,13 @@ class Category(models.Model):
 
 class Platform(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     def __str__(self):
         return self.name
     
 class Videogame(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=250)
+    title = models.CharField(max_length=250, unique=True)
     description = models.TextField(help_text='A brief description of the videogame.')
     categories = models.ManyToManyField(Category, verbose_name='videogame_category')
     platforms = models.ManyToManyField(Platform, verbose_name='videogame_platform', through='VideogamePlatform')
@@ -52,5 +52,5 @@ class VideogameCollection(models.Model):
 class VideogamePlatform(models.Model):
     videogame = models.ForeignKey(Videogame, on_delete=models.CASCADE)
     platform = models.ForeignKey(Platform, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     release_date = models.DateField(null=True, blank=True)
